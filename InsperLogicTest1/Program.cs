75217398bd5a1e61baa7fae883d11f618f591aca
Eudis <eudes.eds@gmail.com>
2022-07-08 19:17:54 -0300

    Adicionar arquivos de projeto.

---
+++ InsperLogicTest1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InsperLogicTest1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNTimes();
+        }
+
+        public static void PrintNTimes()
+        {
+            Console.WriteLine("Olá. \nInsira um valor para N ( inteiro maior que zero ) :");
+            int n = 0;
+            var text = Console.ReadLine();
+            int.TryParse(text, out n);
+
+            if (n > 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    for(int j = 1; j <= n-i; j++)
+                        Console.Write(" ");
+                    for(int k = 1; k <= i; k++)
+                        Console.Write("@");
+                    Console.WriteLine("\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor incorreto. Informe um numero inteiro maior que zero :");
+                PrintNTimes();
+            }
+        }
+    }
+}
